be676ad59812ca456aefcf04e7f01ebe893a082d
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-15 16:47:25 -0300

    agregoboostrap

--- MiPortfolio/src/app/app-routing.module.ts
+++ MiPortfolio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresopagComponent } from './componente/ingresopag/ingresopag.component';
+import { RutaComponent } from './componente/ruta/ruta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RutaComponent}
+  {path: 'ingreso', component: IngresopagComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MiPortfolio/src/app/app.module.ts
+++ MiPortfolio/src/app/app.module.ts
@@ -1,41 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import  {  NgCircleProgressModule  }  from  'ng-circle-progress';
+import { RutaComponent } from './componente/ruta/ruta.component';
 import { BannerComponent } from './componente/banner/banner.component';
-import { LogoAPComponent } from './componente/logo-ap/logo-ap.component';
-import { HeaderComponent } from './componente/header/header.component';
-import { InfopersonalComponent } from './componente/infopersonal/infopersonal.component';
+import { CirculosComponent } from './componente/circulos/circulos.component';
 import { EducacionComponent } from './componente/educacion/educacion.component';
 import { ExperienciaLaboralComponent } from './componente/experiencia-laboral/experiencia-laboral.component';
-import { CirculosComponent } from './componente/circulos/circulos.component';
-import { ProyectosComponent } from './componente/proyectos/proyectos.component';
 import { FooterComponent } from './componente/footer/footer.component';
-import { IngresopagComponent } from './componente/ingresopag/ingresopag.component';
-import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HeaderComponent } from './componente/header/header.component';
+import { InfopersonalComponent } from './componente/infopersonal/infopersonal.component';
+//import { IngresoComponent } from './componente/ingresopag/ingresopag.component';
+import { LogoAPComponent } from './componente/logo-ap/logo-ap.component';
+import { ProyectosComponent } from './componente/proyectos/proyectos.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BannerComponent,
-    LogoAPComponent,
-    HeaderComponent,
-    InfopersonalComponent,
-    EducacionComponent,
-    ExperienciaLaboralComponent,
-    CirculosComponent,
-    ProyectosComponent,
-    FooterComponent,
-    IngresopagComponent
+BannerComponent,
+CirculosComponent,
+EducacionComponent,
+ExperienciaLaboralComponent,
+FooterComponent,
+HeaderComponent,
+InfopersonalComponent,
+//IngresoComponent,
+LogoAPComponent,
+ProyectosComponent,
+    RutaComponent
   ],
   imports: [
     BrowserModule,
+   AppRoutingModule,
     NgCircleProgressModule . forRoot ( { }),
-    AppRoutingModule
+   // HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  //  InterceptorService
+  ],
+ // bootstrap: [AppComponent]
 })
 export class AppModule { }
