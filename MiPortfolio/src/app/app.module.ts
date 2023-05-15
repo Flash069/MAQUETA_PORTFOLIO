ca390ee5a8fb8f8cd6031ab482d6bff48465df0a
BAUDRACCO_FLASH069 <marubaudracco@gmail.com>
2023-05-15 15:33:56 -0300

    copiofrontconpagingreso

--- MiPortfolio/src/app/app.module.ts
+++ MiPortfolio/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './componente/banner/banner.component';
+import { LogoAPComponent } from './componente/logo-ap/logo-ap.component';
+import { HeaderComponent } from './componente/header/header.component';
+import { InfopersonalComponent } from './componente/infopersonal/infopersonal.component';
+import { EducacionComponent } from './componente/educacion/educacion.component';
+import { ExperienciaLaboralComponent } from './componente/experiencia-laboral/experiencia-laboral.component';
+import { CirculosComponent } from './componente/circulos/circulos.component';
+import { ProyectosComponent } from './componente/proyectos/proyectos.component';
+import { FooterComponent } from './componente/footer/footer.component';
+import { IngresopagComponent } from './componente/ingresopag/ingresopag.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    LogoAPComponent,
+    HeaderComponent,
+    InfopersonalComponent,
+    EducacionComponent,
+    ExperienciaLaboralComponent,
+    CirculosComponent,
+    ProyectosComponent,
+    FooterComponent,
+    IngresopagComponent
   ],
   imports: [
     BrowserModule,
---
+++ MiPortfolio/src/app/componente/ingresopag/ingresopag.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresopag',
+  templateUrl: './ingresopag.component.html',
+  styleUrls: ['./ingresopag.component.css']
+})
+
+export class IngresopagComponent {}
+//implements OnInit{
+ // isLogged = false;
+  //isLogginFail = false;
+//  loginUsuario : loginUsuario;
+ // nombreUsuario : string;
+//  password : string;
+ // roles : string [] = [];
+//errMsj: string;
+
+//constructor(private tokenService: TokenService, private authService: AuthService, private router: Router){
+  //ngOnInit(): void {
+    //if (this.tokenService.getToken())
+    //{
+      //this.isLogged = true;
+//    this.isLogginFail = false; 
+//    this.roles = this.tokenService.getAuthorities();
+ // }
+//}
+ //onLogin(): void {
+   //   this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+    //   this.authService.login(this.loginUsuario).subscribe(data => { 
+    //      this.isLogged = true;
+    //    this.isLogginFail = false;
+    //  this.tokenService.setToken(data.token);
+  //    this.tokenService.setUserName(data.nombreUsuario);
+ //     this.tokenService.setAuthorities(data.authorities);
+ //     this.roles = data.authorities;
+  //    this.Router.navigate([''])
+  //  }
+//}
+//err => {this.isLogged = false;
+ // this.isLogginFail =true;
+  //this.errMsj = err.error.mensaje;
+  //console.log(this.errMsj);
+  //}
+//}
+
+//function onLogin() {
+  //throw new Error('Function not implemented.');
+//}
+
+//function ngOnInit() {
+  //throw new Error('Function not implemented.');
+//}
